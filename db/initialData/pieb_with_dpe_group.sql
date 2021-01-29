c2a0953f9f38153a30356b1352475013a0e530c9
Liangxiaojun <fatalxiao@163.com>
2021-01-29 10:09:05 +0800

    update

--- db/initialData/pieb_with_dpe_group.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO pieb_with_dpe.pieb_with_dpe_group (id, name) VALUES (1, '30Min');
-INSERT INTO pieb_with_dpe.pieb_with_dpe_group (id, name) VALUES (2, '40Min');
-INSERT INTO pieb_with_dpe.pieb_with_dpe_group (id, name) VALUES (3, '50Min');
-INSERT INTO pieb_with_dpe.pieb_with_dpe_group (id, name) VALUES (4, '60Min');
-INSERT INTO pieb_with_dpe.pieb_with_dpe_group (id, name) VALUES (5, 'Other');
