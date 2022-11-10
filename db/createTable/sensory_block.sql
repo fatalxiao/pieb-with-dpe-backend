8f16243aa711d3213fe8defbab1b7e663bead1ee
liangxiaojun <fatalxiao@163.com>
2022-11-10 14:04:56 +0800

    update sensory_block.sql

--- db/createTable/sensory_block.sql
+++ db/createTable/sensory_block.sql
@@ -1,9 +1,7 @@
 CREATE TABLE sensory_block (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	NAME VARCHAR ( 20 ) NULL,
-
-	VALUE
-		INT NULL,
-		type INT NULL COMMENT '阻滞平面类型，1：头端阻滞平面，2：尾端阻滞平面',
+	VALUE INT NULL,
+	type INT NULL COMMENT '阻滞平面类型，1：头端阻滞平面，2：尾端阻滞平面',
 	CONSTRAINT sensory_block_id_uindex UNIQUE ( id )
-	) COMMENT '阻滞平面' ENGINE = INNODB CHARSET = utf8;
+) COMMENT '阻滞平面' ENGINE = INNODB CHARSET = utf8;
